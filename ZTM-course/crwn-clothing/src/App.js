9c3102c09c79129bad50c71a4761906140a86e0c
Luca Luzzi <luzziluca6@gmail.com>
2022-12-27 18:40:01 -0300

    updating our directory page with directory component and category item component

---
+++ ZTM-course/crwn-clothing/src/App.js
@@ -0,0 +1,7 @@
+import { Directory } from './components/directory/directory.component';
+
+export const App = () => {
+  return (
+    <Directory />
+  );
+}
---
+++ ZTM-course/crwn-clothing/src/components/category-item/category-item.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './category-item.styles.scss'
+
+export const CategoryItem = ({ title, imageUrl}) => {
+  return (
+    <div className='category-container'>
+        <div className='background-image' style={{ backgroundImage: `url(${ imageUrl })` }} />
+        <div className='category-body-container'>
+        <h2>{ title }</h2>
+        <p>Show now</p>
+        </div>
+    </div>
+  )
+}
